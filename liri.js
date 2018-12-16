78e15c0029a75ed834ffae909171b737c95396c1
Requ Amer <requamer@Requs-MacBook-Pro.local>
2018-12-16 10:59:18 -0800

    edition

--- liri.js
+++ liri.js
@@ -1,151 +1,112 @@
-// Load exports from keys.js file which has Twitter auth keys
-var keys = require("./keys.js");
-var twitterCredentials = keys.twitterKeys;
+//Below code will get configuration information from .env file
+require("dotenv").config();
+//module to read file from random.txt
+var fs = require("fs");
+var keys = require('./keys');
+var Spotify = require('node-spotify-api');
+//added to format table 
+var cTable = require('console.table');
+//module to make request
+var request = require('request');
+//module for date
+var moment = require('moment');
 
-// node liri.js [ command ] [ query - optional ]
-var command = process.argv[2];
-var query = process.argv[3];
 
-// Functions for 3 main functions of the app
-// 	--> do-what-it-says requires the use of functions
-var myTweets = function() {
-	// Load twitter module from npm
-	var Twitter = require('twitter');
 
-	// From exports of keys.js file
-	var client = new Twitter({
-		consumer_key: twitterCredentials.consumer_key,
-		consumer_secret: twitterCredentials.consumer_secret,
-		access_token_key: twitterCredentials.access_token_key,
-		access_token_secret: twitterCredentials.access_token_secret
-	});
+var spotify = new Spotify({
+    id: keys.spotify.id,
+    secret: keys.spotify.secret
+});
+//Output required information
+// Name of the venue
+// Venue location
+// Date of the Event (use moment to format this as "MM/DD/YYYY")  
 
-	// Twitter API parameters
-	var params = {
-		screen_name: 'GiantGreenLion',
-		count: 20
-	};
+//  argument 3 = concert-this or spotify-this-song or movie-this or do-what-it-says
+if (process.argv[2] == 'concert-this') {
 
-	// GET request for last 20 tweets on my account's timeline
-	client.get('statuses/user_timeline', params, function(error, tweets, response) {
-		if(error) { // if there IS an error
-			console.log('Error occurred: ' + error);
-		} else { // if there is NO error
-	  	console.log("My 20 Most Recent Tweets");
-	  	console.log("");
+    var artist = process.argv.slice(3).join(" ")
+    // console.log(artist);
 
-	  	for(var i = 0; i < tweets.length; i++) {
-	  		console.log("( #" + (i + 1) + " )  " + tweets[i].text);
-	  		console.log("Created:  " + tweets[i].created_at);
-	  		console.log("");
-	  	}
-	  }
-	});
+    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=dffc52a8a4eb7aba919d7920e96d7b94";
+
+    request(queryURL, function (error, response, body) {
+        if (error) console.log(error);
+        var result = JSON.parse(body)[0];
+        console.log("##################################################################");
+        console.log("Using BandsofTown API ");
+        console.log("Venue name:" + result.venue.name);
+        console.log("Venue location: " + result.venue.city);
+        console.log("Date of Event: " + moment(result.datetime).format("MM/DD/YYYY"));
+        console.log("##################################################################");
+
+        //add txt file 
+        fs.appendFile('log.txt', result.venue.name + result.venue.city + moment(result.datetime).format("MM/DD/YYYY"),(err) => {
+            if (err) throw err;
+            console.log('The "data to append" was appended to file!');
+        });
+            
+    });
+
+} else if (process.argv[2] == 'spotify-this-song') {
+
+    var songName = process.argv.slice(3).join(" ");
+    if (songName == undefined) {
+        songName = "Song is not defined";
+    }
+    spotify.search({ type: 'track', query: songName, limit: 10 }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        var tableArray = [];
+        for (var i = 0; i < data.tracks.items.length; i++) {
+            var result = {
+                artist: data.tracks.items[i].album.artists[0].name,
+                song_name: data.tracks.items[i].name,
+                album_name: data.tracks.items[i].album.name,
+                preview_url: data.tracks.items[i].preview_url
+
+                
+            }
+            tableArray.push(result);
+        }
+        var table = cTable.getTable(tableArray);
+        console.log(table);
+
+
+    });
+
+} else if (process.argv[2] == 'movie-this') {
+    var movieName = process.argv.slice(3).join(" ");
+
+    if (movieName == undefined) {
+        movieName = "Movie is undefined err";
+    }
+
+    request('http://www.omdbapi.com/?i=tt3896198&apikey=55e8eecb&t=' + process.argv[3], function (error, response, body) {
+
+        var result = JSON.parse(body);
+        console.log("Title :" + result.Title);
+        console.log("Year :" + result.Released);
+        console.log("IMDB Rating :" + result.imdbRating);
+        console.log("Rotten Tomatoes :" + result.Ratings[1].Value);
+        console.log("Country :" + result.Country);
+        console.log("Language :" + result.Language);
+        console.log("Movie Plot :" + result.Plot);
+        console.log("Actors :" + result.Actors);
+
+    });
+
+} else if (process.argv[2] == 'do-what-it-says') {
+    readSpotifyTxtFile();
 }
 
-var spotifyThisSong = function(trackQuery) {
-	// Load Spotify npm package
-	var spotify = require('spotify');
-
-	// if no trackQuery is passed in, then we will be querying for this particular song
-	if(trackQuery === undefined) {
-		trackQuery = "the sign ace of base";
-	}
-
-	// Spotify API request (if an object is returned, output the first search result's artist(s), song, preview link, and album)
-	spotify.search({ type: 'track', query: trackQuery }, function(error, data) {
-	    if(error) { // if error
-	        console.log('Error occurred: ' + error);
-	    } else { // if no error
-	    	// For loop is for when a track has multiple artists
-				for(var i = 0; i < data.tracks.items[0].artists.length; i++) {
-					if(i === 0) {
-						console.log("Artist(s):    " + data.tracks.items[0].artists[i].name);
-					} else {
-						console.log("              " + data.tracks.items[0].artists[i].name);
-					}
-				}
-				console.log("Song:         " + data.tracks.items[0].name);
-				console.log("Preview Link: " + data.tracks.items[0].preview_url);
-				console.log("Album:        " + data.tracks.items[0].album.name);
-	    }
-	 
-	 		
-	});
-}
-
-var movieThis = function(movieQuery) {
-	// Load request npm module
-	var request = require("request");
-
-	// if query that is passed in is undefined, Mr. Nobody becomes the default
-	if(movieQuery === undefined) {
-		movieQuery = "mr nobody";
-	}
-
-	// HTTP GET request
-	request("http://www.omdbapi.com/?t=" + movieQuery + "&y=&plot=short&r=json", function(error, response, body) {
-	  if (!error && response.statusCode === 200) {
-	    console.log("* Title of the movie:         " + JSON.parse(body).Title);
-	    console.log("* Year the movie came out:    " + JSON.parse(body).Year);
-	    console.log("* IMDB Rating of the movie:   " + JSON.parse(body).imdbRating);
-	    console.log("* Country produced:           " + JSON.parse(body).Country);
-	    console.log("* Language of the movie:      " + JSON.parse(body).Language);
-	    console.log("* Plot of the movie:          " + JSON.parse(body).Plot);
-	    console.log("* Actors in the movie:        " + JSON.parse(body).Actors);
-
-	    // For loop parses through Ratings object to see if there is a RT rating
-	    // 	--> and if there is, it will print it
-	    for(var i = 0; i < JSON.parse(body).Ratings.length; i++) {
-	    	if(JSON.parse(body).Ratings[i].Source === "Rotten Tomatoes") {
-	    		console.log("* Rotten Tomatoes Rating:     " + JSON.parse(body).Ratings[i].Value);
-	    		if(JSON.parse(body).Ratings[i].Website !== undefined) {
-	    			console.log("* Rotten Tomatoes URL:        " + JSON.parse(body).Ratings[i].Website);
-	    		}
-	    	}
-	    }
-	  }
-	});
-}
-
-// App functionality due to user input
-if(command === "my-tweets") {
-	myTweets();
-} else if(command === "spotify-this-song") {
-	spotifyThisSong(query);
-} else if(command === "movie-this") {
-	movieThis(query);
-} else if(command === "do-what-it-says") {
-	// App functionality from file read / loads fs npm package
-	var fs = require("fs");
-
-	fs.readFile("random.txt", "utf-8", function(error, data) {
-		var command;
-		var query;
-
-		// If there is a comma, then we will split the string from file in order to differentiate between the command and query
-		// 	--> if there is no comma, then only the command is considered (my-tweets)
-		if(data.indexOf(",") !== -1) {
-			var dataArr = data.split(",");
-			command = dataArr[0];
-			query = dataArr[1];
-		} else {
-			command = data;
-		}
-
-		// After reading the command from the file, decides which app function to run
-		if(command === "my-tweets") {
-			myTweets();
-		} else if(command === "spotify-this-song") {
-			spotifyThisSong(query);
-		} else if(command === "movie-this") {
-			movieThis(query);
-		} else { // Use case where the command is not recognized
-			console.log("Command from file is not a valid command! Please try again.")
-		}
-	});
-} else if(command === undefined) { // use case where no command is given
-	console.log("Please enter a command to run LIRI.")
-} else { // use case where command is given but not recognized
-	console.log("Command not recognized! Please try again.")
+function readSpotifyTxtFile() {
+    fs.readFile('random.txt',"utf8", (err, data) => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        console.log(data)
+    })
 }
